ee1e51fdb9e03ece624a0ba43db09d8d1cf77905
Malnati <ricardomalnati@gmail.com>
2023-10-16 06:08:47 +0200

    feat Added new formula

---
+++ anyhooks.rb
@@ -0,0 +1,23 @@
+class Anyhooks < Formula
+  desc "Manages hooks, encryption, and language settings for AnyHooks"
+  homepage "https://github.com/Malnati/AnyHooks"
+  url "https://github.com/Malnati/AnyHooks/archive/refs/tags/v0.2-alpha.tar.gz"
+  sha256 "648fea19c1fa351ca337b49b0d0311038eac6ae916d6041c9f7e83d7fa1db17d"
+  license "MIT"  # Update this based on your project's license
+
+  depends_on "curl"
+  depends_on "jq"
+  depends_on "openssl"
+
+  def install
+    bin.install "rc/anyhooks"
+    bin.install "rc/anyhookslang"
+    bin.install "rc/anyhooksver"
+    bin.install "rc/openaikey"
+  end
+
+  test do
+    system "#{bin}/anyhooks", "--version"
+  end
+end
+
