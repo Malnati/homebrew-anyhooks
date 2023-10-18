ec4568c542c140b27392cc14c55b8eac03dd3277
Malnati <ricardomalnati@gmail.com>
2023-10-18 06:37:54 +0200

    feat changed version to v1.0.0-alpha.tar.gz

--- anyhooks.rb
+++ anyhooks.rb
@@ -1,7 +1,7 @@
 class Anyhooks < Formula
   desc "Manages hooks, encryption, and language settings for AnyHooks"
   homepage "https://github.com/Malnati/AnyHooks"
-  url "https://github.com/Malnati/AnyHooks/archive/refs/tags/v1.0.0-alpha.tar.gz"
+  url "https://github.com/Malnati/AnyHooks/archive/refs/tags/v1.2.0-beta+addremove.tar.gz"
   sha256 "2fe2a759416c5f09778fe41b50ab07fba0cf964c13fb7c0e6a3cf6428e87e3a2"
   license "MIT" # Update this based on your project's license
 
